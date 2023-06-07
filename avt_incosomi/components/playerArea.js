4525460385aa52097343fe065e5a8f79284f49da
Ricardo Lopes <ricardo.d.r.lopes@gmail.com>
2023-06-07 23:09:19 +0200

    using tables now

--- avt_incosomi/components/playerArea.js
+++ avt_incosomi/components/playerArea.js
@@ -1,11 +1,10 @@
 import PlayerBar from "@/components/playerBar";
-import { useState, useEffect, useRef } from "react";
-import { PlusSmallIcon} from "@heroicons/react/24/solid";
+import {useState, useEffect, useRef} from "react";
+import {PlusSmallIcon} from "@heroicons/react/24/solid";
 
 export default function PlayerArea() {
-    const [barIds, setBarIds] = useState([""]);
+    const [barIds, setBarIds] = useState([]);
     const [lastBarId, setLastBarId] = useState(0);
-    const headerRef = useRef();
 
     const handleAddPlayerBar = () => {
         setLastBarId(lastBarId + 1);
@@ -13,47 +12,29 @@ export default function PlayerArea() {
     };
 
     const handleDeletePlayerBar = (barId) => {
-        const list = [...barIds];
-        let indexToBeRemoved = list.indexOf(barId);
-        list.splice(indexToBeRemoved, 1);
-        setBarIds(list);
+        setBarIds(barIds.filter((id) => id !== barId));
     };
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const header = headerRef.current;
-            const tableRect = header.getBoundingClientRect();
-
-            if (tableRect.top <= 0 && tableRect.bottom >= 0) {
-                header.classList.add("sticky", "top-0", "z-10", "bg-white");
-            } else {
-                header.classList.remove("sticky", "top-0", "z-10", "bg-white");
-            }
-        };
-
-        window.addEventListener("scroll", handleScroll);
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    return (
-        <div id="PlayerArea" className="relative">
-            <div ref={headerRef}>
-                <div className="grid grid-cols-4">
-                    <div>Column 1 Header</div>
-                    <div>Column 2 Header</div>
-                    <div>Column 3 Header</div>
-                    <div>Column 4 Header</div>
-                </div>
-            </div>
+    return (<div id="PlayerArea">
+        <table className="table-auto">
+            <thead>
+            <tr>
+                <th>Waveform</th>
+                <th>Import</th>
+                <th>Play</th>
+                <th>Delete</th>
+                <th>High</th>
+                <th>Mid</th>
+                <th>Low</th>
+            </tr>
+            </thead>
+            <tbody>
             {barIds.map((barId) => (
-                <PlayerBar key={barId} deleteHandler={() => handleDeletePlayerBar(barId)} />
-            ))}
-            <button className="my-1 rounded-full btn btn-success" onClick={handleAddPlayerBar}>
-                <PlusSmallIcon className="w-6 h-6"></PlusSmallIcon>
-            </button>
-        </div>
-    );
+                <PlayerBar key={barId} id={barId} deleteHandler={() => handleDeletePlayerBar(barId)}/>))}
+            </tbody>
+        </table>
+        <button className="my-1 rounded-full btn btn-success" onClick={handleAddPlayerBar}>
+            <PlusSmallIcon className="h-6 w-6"/>
+        </button>
+    </div>);
 }
